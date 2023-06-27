73877a1cc40acb7995b646dafb2183631370fd2f
Cristian Barajas <cristian.barajas@jarsa.com>
2023-06-27 15:23:27 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Jarsa
+# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Comercial User Access",
+    "author": "Jarsa",
+    "website": "https://www.jarsa.com",
+    "license": "LGPL-3",
+    "category": "Installer",
+    "version": "15.0.1.0.0",
+    "depends": [
+        "base",
+    ],
+    "data": [
+    ],
+}
---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2023 Jarsa
+# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import prueva
+print("hola")
